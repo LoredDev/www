9dd19b75bdd332973fe1a47c8cd99cd63229b874
Guz013 <43732358+Guz013@users.noreply.github.com>
2023-06-14 13:38:36 -0300

    feat: ✨ initial commit
    Created (mostly) temporally website, forked from Org013/www.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	</script>
+
+<section un-max-w="60ch" un-m="y-auto" un-font="cal">
+	<figure
+		un-w="5ch"
+		un-justify-self="lg:end center"
+		un-place-self="lg:end center"
+		un-m="0"
+	>
+		<figcaption un-hidden>Lored Logo</figcaption>
+		<LogoLightSvg />
+	</figure>
+
+	Welcome to Lored! The incubator name and organization of all libraries, tools,
+	configs, and resources made for developers by
+	[@Guz013](https://github.com/Guz013).
+	<!--  -->
+	This is a project created out of passion
+	and a need for a dedicated space to share and store of all repositories
+	related to improving, streamlining, strengthening, speeding up and simply
+	making the developer experience more satisfying and fun.
+	<!--  -->
+	Feel free to explore, use, modify and contribute any repository you find interesting
+	or useful. All of them are licensed under permissive licenses.
+
+	<p>
+		<em>
+			<small>
+				<samp>
+					This is a new and premature project <del>and developer</del> with not a
+					lot of experience, but with a lot of ideas for the future. And what's the
+					best way to learn something? Doing it!
+				</samp>
+			</small>
+		</em>
+	</p>
+
+	<hr un-w="10ch" un-m="y-5" un-border="gray1 solid 1" />
+
+	<div>
+		<a href="https://guz.one">
+			<figure
+				un-w="5ch"
+				un-text="dark:white"
+				un-justify-self="lg:end center"
+				un-place-self="lg:end center"
+				un-m="0"
+			>
+				<figcaption un-hidden>Guz013 Logo</figcaption>
+				<Logo013Svg />
+			</figure>
+		</a>
+	</div>
+</section>
